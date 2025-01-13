c961b2bcfaae66f814e552684beabeb3577d927b
bcaliva21 <bcaliva21@gmail.com>
2025-01-13 16:13:40 -0600

    feat: add functions to complete dps calculations

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -16,15 +17,22 @@ func main() {
 	// Ask the user a question
 	question := "What is your base damage?"
 	damageScaling.baseDamage = collectInt(question)
+	fmt.Println("\nCurrent damage scaling values:", damageScaling)
 
-	// Display the answer
-	fmt.Println("Current damage scaling values:", damageScaling)
+	damageScaling.increasedDamageTotal = collectIncreasedDamageValues()
+	fmt.Println("\nCurrent damage scaling values:", damageScaling)
 
-	increasedDamageValue := collectIncreasedDamageValues("Increased damage values")
-	fmt.Printf("The final damage value is: %.2f\n", increasedDamageValue)
+	damageScaling.moreDamageTotal = collectMoreDamageValues()
+	fmt.Println("\nCurrent damage scaling values:", damageScaling)
 
-	moreDamageValue := collectMoreDamageValues("More damage values")
-	fmt.Printf("%.2f", moreDamageValue)
+	damageScaling.addedDamageTotal = collectAddedDamageValues()
+	fmt.Println("\nCurrent damage scaling values:", damageScaling)
+
+	damageScaling.hitRate = collectHitRate()
+	fmt.Println("\nCurrent damage scaling values:", damageScaling)
+
+	calculateDamagePerSecond(&damageScaling)
+	fmt.Println("\nCurrent damage scaling values:", damageScaling)
 }
 
 type DamageScaling struct {
@@ -33,21 +41,84 @@ type DamageScaling struct {
 	gainedDamageTotal         int
 	moreDamageTotal           float32
 	increasedDamageTotal      float32
-	hitRate                   int
+	hitRate                   float32
 	criticalStrikeDamageTotal int
 	criticalStrikeChanceTotal int
+	damagePerSecond           float32
 }
 
 func convertToMultiplier(percentage int) float32 {
 	return float32(percentage)/100 + 1
 }
 
-func collectMoreDamageValues(category string) float32 {
-	fmt.Println("Enter values for ", category)
+func calculateDamagePerSecond(damageScaling *DamageScaling) {
+	totalBaseDamage := float32(damageScaling.baseDamage + damageScaling.addedDamageTotal)
+	damageScaling.damagePerSecond = totalBaseDamage
+	damageScaling.damagePerSecond *= damageScaling.increasedDamageTotal
+	damageScaling.damagePerSecond *= damageScaling.moreDamageTotal
+	damageScaling.damagePerSecond *= damageScaling.hitRate
+}
+
+func collectHitRate() float32 {
+	fmt.Println("Enter your attacks/casts per second")
+
+	var userInput string
+	fmt.Print("Attack/cast per second: ")
+	fmt.Scanln(&userInput)
+
+	hitRateValue, err := strconv.ParseFloat(userInput, 32)
+	if err != nil {
+		fmt.Println("Invalid input, please enter a valid integer value.")
+	}
+	return float32(hitRateValue)
+}
+
+func collectAddedDamageValues() int {
+	fmt.Println("Enter values for added damage")
+	var added int
+
+	for {
+		// Prompt the user to enter low and high values
+		fmt.Println("\nEnter a low value and a high value, or type 'done' to finish:\n")
+
+		var lowInput, highInput string
+		fmt.Print("Low value: ")
+		fmt.Scanln(&lowInput)
+
+		// Check if the user wants to finish
+		if lowInput == "done" {
+			break
+		}
+
+		fmt.Print("High value: ")
+		fmt.Scanln(&highInput) // Convert user input to integer
+
+		if highInput == "done" {
+			break
+		}
+		lowValue, errLow := strconv.Atoi(lowInput)
+		highValue, errHigh := strconv.Atoi(highInput)
+
+		if errLow != nil || errHigh != nil {
+			fmt.Println("Invalid input, please enter a valid integer value.")
+			continue
+		}
+
+		rawAvg := float64((lowValue + highValue) / 2)
+		avgValue := int(math.Floor(rawAvg))
+
+		added += avgValue
+	}
+
+	return added
+}
+
+func collectMoreDamageValues() float32 {
+	fmt.Println("Enter more damage values: ")
 
 	multiplier := float32(1)
 	for {
-		fmt.Println("Enter a value (percentage as integer, or type 'done' to finish):")
+		fmt.Println("\nEnter a value (percentage as integer, or type 'done' to finish):\n")
 		var userInput string
 		fmt.Scanln(&userInput)
 
@@ -73,14 +144,11 @@ func collectMoreDamageValues(category string) float32 {
 	return multiplier
 }
 
-func collectIncreasedDamageValues(category string) float32 {
-	// Ask user for input type (individual or total)
-	// Normalize input type (to handle casing)
-	// Handle individual values input
-	fmt.Println("Enter values for ", category)
+func collectIncreasedDamageValues() float32 {
+	fmt.Println("Enter increased damage values: ")
 	var sum float32
 	for {
-		fmt.Println("Enter a value (percentage as integer, or type 'done' to finish):")
+		fmt.Println("\nEnter a value (percentage as integer, or type 'done' to finish):\n")
 		var userInput string
 		fmt.Scanln(&userInput)
 
@@ -107,7 +175,7 @@ func collectIncreasedDamageValues(category string) float32 {
 
 func (d DamageScaling) String() string {
 	return fmt.Sprintf(
-		"{\n  baseDamage: %d,\n  addedDamageTotal: %d,\n  gainedDamageTotal: %d,\n  moreDamageTotal: %.2f,\n  increasedDamageTotal: %d,\n  hitRate: %d,\n  criticalStrikeDamageTotal: %d,\n  criticalStrikeChanceTotal: %d\n}",
+		"{\n  baseDamage: %d,\n  addedDamageTotal: %d,\n  gainedDamageTotal: %d,\n  moreDamageTotal: %.2f,\n  increasedDamageTotal: %.2f,\n  hitRate: %.2f,\n  criticalStrikeDamageTotal: %d,\n  criticalStrikeChanceTotal: %d,\n  damagePerSecond: %.2f\n}",
 		d.baseDamage,
 		d.addedDamageTotal,
 		d.gainedDamageTotal,
@@ -116,6 +184,7 @@ func (d DamageScaling) String() string {
 		d.hitRate,
 		d.criticalStrikeDamageTotal,
 		d.criticalStrikeChanceTotal,
+		d.damagePerSecond,
 	)
 }
 
