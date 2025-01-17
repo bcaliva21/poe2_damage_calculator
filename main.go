38d970dd997e5566148fb3291e6db2461619464b
bcaliva21 <bcaliva21@gmail.com>
2025-01-17 11:45:02 -0600

    feat: move styles out of index.html into styles.css

--- main.go
+++ main.go
@@ -4,13 +4,12 @@ import (
     "fmt"
     "math"
     "net/http"
-    "os"
     "strconv"
     "strings"
 )
 
 func main() {
-    // Start the HTTP server to serve the HTML file
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         http.ServeFile(w, r, "index.html")
     })
@@ -51,7 +50,6 @@ func handleFormSubmit(w http.ResponseWriter, r *http.Request) {
     if r.Method == http.MethodPost {
         var damageScaling DamageScaling
 
-        // Parse form values
         baseDamage, _ := strconv.Atoi(r.FormValue("baseDamage"))
         addedDamage := r.FormValue("addedDamage")
         moreDamage := r.FormValue("moreDamage")
@@ -66,7 +64,6 @@ func handleFormSubmit(w http.ResponseWriter, r *http.Request) {
 
         calculateDamagePerSecond(&damageScaling)
 
-        // Display the results
         fmt.Fprintf(w, "Damage Per Second: %.2f", damageScaling.damagePerSecond)
     } else {
         http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
@@ -74,9 +71,9 @@ func handleFormSubmit(w http.ResponseWriter, r *http.Request) {
 }
 
 func collectAddedDamageValuesFromString(input string) int {
-	if input == "" {
-		return 0
-	}
+    if input == "" {
+        return 0
+    }
     var added int
     values := strings.Split(input, ",")
     for i := 0; i < len(values); i += 2 {
@@ -91,9 +88,9 @@ func collectAddedDamageValuesFromString(input string) int {
 
 func collectMoreDamageValuesFromString(input string) float32 {
     multiplier := float32(1)
-	if input == "" {
-		return multiplier
-	}
+    if input == "" {
+        return multiplier
+    }
     values := strings.Split(input, ",")
     for _, valueStr := range values {
         value, _ := strconv.Atoi(strings.TrimSpace(valueStr))
@@ -105,9 +102,9 @@ func collectMoreDamageValuesFromString(input string) float32 {
 
 func collectIncreasedDamageValuesFromString(input string) float32 {
     var sum float32
-	if input == "" {
-		return float32(1)
-	}
+    if input == "" {
+        return float32(1)
+    }
     values := strings.Split(input, ",")
     for _, valueStr := range values {
         value, _ := strconv.Atoi(strings.TrimSpace(valueStr))
@@ -115,59 +112,4 @@ func collectIncreasedDamageValuesFromString(input string) float32 {
         sum += quotient
     }
     return sum + float32(1)
-}
-
-func (d DamageScaling) String() string {
-    return fmt.Sprintf(
-        "{\n  baseDamage: %d,\n  addedDamageTotal: %d,\n  gainedDamageTotal: %d,\n  moreDamageTotal: %.2f,\n  increasedDamageTotal: %.2f,\n  hitRate: %.2f,\n  criticalStrikeDamageTotal: %d,\n  criticalStrikeChanceTotal: %d,\n  damagePerSecond: %.2f\n}",
-        d.baseDamage,
-        d.addedDamageTotal,
-        d.gainedDamageTotal,
-        d.moreDamageTotal,
-        d.increasedDamageTotal,
-        d.hitRate,
-        d.criticalStrikeDamageTotal,
-        d.criticalStrikeChanceTotal,
-        d.damagePerSecond,
-    )
-}
-
-// Helper function to format slices
-func formatSlice(slice []float32) string {
-    var str []string
-    for _, val := range slice {
-        str = append(str, fmt.Sprintf("%.2f", val))
-    }
-    return fmt.Sprintf("[%s]", strings.Join(str, ", "))
-}
-
-func collectInt(question string) int {
-    fmt.Printf("%s\n", question)
-
-    // Read the input from the user
-    var input int
-    _, err := fmt.Scanln(&input)
-    if err != nil {
-        fmt.Println("Error reading input:", err)
-        os.Exit(1)
-    }
-
-    // Return the user's input, trimming extra spaces
-    return input
-}
-
-// askQuestion displays a question and waits for the user input
-func askQuestion(question string) string {
-    fmt.Printf("%s\n", question)
-
-    // Read the input from the user
-    var input string
-    _, err := fmt.Scanln(&input)
-    if err != nil {
-        fmt.Println("Error reading input:", err)
-        os.Exit(1)
-    }
-
-    // Return the user's input, trimming extra spaces
-    return strings.TrimSpace(input)
 }
